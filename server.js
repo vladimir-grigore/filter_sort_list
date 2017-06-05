848cb92cc2163beb8d62454098ed61029fb087a7
Vladimir Grigore <vladimirgrigore@Vladimirs-MacBook-Pro.local>
2017-06-04 23:14:36 -0700

    Update .gitignore file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const apiRoutes = require("./api_calls");
